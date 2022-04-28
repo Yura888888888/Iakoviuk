3f7e0aff77a5d24129a65227881afd80cba0922f
Yura888888888 <99320075+Yura888888888@users.noreply.github.com>
2022-04-28 15:13:58 +0300

    Add files via upload

---
+++ Project5.2.java
@@ -0,0 +1,11 @@
+package Project5.2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Батько: Яковюк Дмитро Дмитрович, 1978 року народження, військовослужбовець");
+		System.out.println("Мати: Яковюк Марія Володимирівна, 1981 року народження, військовослужбовець");
+		System.out.println("Сестра: Яковюк Юлія Дмитрівна, 2009 року народження, школяр");
+	}
+
+}
